035a2f383fcd1f4c9cf591db4443410ffb42a228
Easy solutions <Easy solutions@ezs013>
2017-08-03 18:02:06 +0530

    SOP

--- Maven_Selenium/src/test/Test1.java
+++ Maven_Selenium/src/test/Test1.java
@@ -6,6 +6,6 @@ public class Test1 {
   @Test(priority=1)
   public void f() {
 	  System.out.println("Test 1 log");
-	  System.out.println("Thiyagu work area from github::");
+	  System.out.println("Thiyagu work area from github: to jenkins");
   }
 }
