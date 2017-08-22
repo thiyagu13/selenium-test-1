303428b5e76aeb28d46b7e44e04c91171c31e14e
thiyagu13 <thiyagu@easysolutions.in>
2017-08-22 11:49:53 +0530

    Update Selenium_Test1.java

--- Maven_Selenium/src/selenium/Selenium_Test1.java
+++ Maven_Selenium/src/selenium/Selenium_Test1.java
@@ -1,15 +1,19 @@
 package selenium;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Selenium_Test1 {
 	@Test
 	public void testgooglrsearch(){
-	System.setProperty("webdriver.chrome.driver", ".\\chromedriver.exe");
-	WebDriver driver = new ChromeDriver();
+		System.setProperty(Constant.GECKO_DRIVER, Constant.GECKO_DRIVER_PATH);
+		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+		capabilities.setCapability("marionette", true);
+		WebDriver driver = new FirefoxDriver(capabilities);
+	
 	//it will open the goggle page
 	driver.get("http://google.in");
 	//we expect the title “Google “ should be present
