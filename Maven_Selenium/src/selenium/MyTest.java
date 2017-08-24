363db778fd50f5414cd5f9816118a8dc1cef4072
Easy solutions <Easy solutions@ezs013>
2017-08-24 14:25:47 +0530

    dfgdg

--- Maven_Selenium/src/selenium/MyTest.java
+++ Maven_Selenium/src/selenium/MyTest.java
@@ -16,7 +16,7 @@ import java.util.HashMap;
 
 
 public class MyTest {
-
+/*
    private static HashMap<String, WebDriver> driverObjMap=new HashMap<String, WebDriver>();
     WebDriver driver = new ChromeDriver();
     public static WebDriver gerDriverDetails(String className){
@@ -25,9 +25,9 @@ public class MyTest {
 
     public static HashMap<String, WebDriver> gerDriverObjMap(){
         return driverObjMap;
-    }
+    }*/
 
-	/*WebDriver driver = Utils.getWebDriverInstance();*/
+	WebDriver driver = Utils.getWebDriverInstance();
 
   
 
