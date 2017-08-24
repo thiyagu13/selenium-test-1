2687d7b664aeba2f582ac6c2f043321a433ac2a2
Easy solutions <Easy solutions@ezs013>
2017-08-24 12:54:30 +0530

    test

--- Maven_Selenium/src/selenium/MyTest.java
+++ Maven_Selenium/src/selenium/MyTest.java
@@ -1,6 +1,7 @@
 package selenium;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
@@ -13,8 +14,8 @@ import java.util.HashMap;
 
 public class MyTest {
 
-    private static HashMap<String, WebDriver> driverObjMap=new HashMap<String, WebDriver>();
-    WebDriver driver;
+   private static HashMap<String, WebDriver> driverObjMap=new HashMap<String, WebDriver>();
+  //  WebDriver driver;
     public static WebDriver gerDriverDetails(String className){
         return driverObjMap.get(className);
     }
@@ -23,16 +24,17 @@ public class MyTest {
         return driverObjMap;
     }
 
+	WebDriver driver = Utils.getWebDriverInstance();
     @BeforeClass
     public void browserInstantiate(){
-    	System.setProperty("webdriver.gecko.driver", "C:\\selenium\\Selenium_Project\\geckodriver.exe");
-		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-		capabilities.setCapability("marionette", true);    	
-		WebDriver driver = new FirefoxDriver(capabilities); 
+    	/*System.setProperty("webdriver.gecko.driver",Constant.GECKO_DRIVER_PATH);
+    	DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+    	capabilities.setCapability("marionette", true);
+    	WebDriver driver = new FirefoxDriver(capabilities);
     	//driver = new ChromeDriver();
-        driverObjMap.put(getClass().getName(),driver);
+       // driverObjMap.put(getClass().getName(),driver);
         driver.manage().window().maximize();
-        System.out.println("Browser Instantiated..");
+        System.out.println("Browser Instantiated..");*/
     }
 
     @Test(priority=1)
