7d51261ff17f1432b2b5f0f26db530f12010e9ff
thiyagu13 <thiyagu@easysolutions.in>
2017-08-24 11:50:13 +0530

    Update MyTest.java

--- Maven_Selenium/src/selenium/MyTest.java
+++ Maven_Selenium/src/selenium/MyTest.java
@@ -1,7 +1,9 @@
 package selenium;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
